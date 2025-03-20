d9100e16c08b9112661a2b00826d1a38ca880108
diegun99 <diegun123456789@gmail.com>
2025-03-19 22:03:01 -0500

    completado paginador con signals en el ts

--- apps/diegun-movie/src/app/features/movies/movies.component.ts
+++ apps/diegun-movie/src/app/features/movies/movies.component.ts
@@ -1,4 +1,4 @@
-import { Component, effect, inject } from '@angular/core';
+import { Component, computed, effect, HostListener, inject } from '@angular/core';
 import { MoviesService } from './movies.service';
 
 @Component({
@@ -7,15 +7,35 @@ import { MoviesService } from './movies.service';
   templateUrl: './movies.component.html'
 })
 export class MoviesComponent {
+  isLoading = computed(()=>this._movieService.isLoading);// computed permite crear una signal a traves de otra signal
+  hasMorePages = computed(()=>this._movieService.hasMorepages);
 private readonly _movieService = inject(MoviesService) /// inyectar read only los servicios y privados
-
 readonly movies = this._movieService.movies;// se crea una propiedad writeblesignal
-constructor(){
- effect(/// ejecuta una funcion segun cambia el signal
-  ()=>{
-console.log(this.movies())
+// para el buscador se usará rxresource que es una nueva api experimental de angular 19
+// paginacion infinita
+// este decorador va a decorarrr un metodo
+@HostListener('window:scroll ')  // con este decorador se controla la paginacion y le pasamos el evento que queremos escuchar
+onScroll() : void {
+  if(this.isLoading() || !this.hasMorePages()){
+    return
   }
- )
+  // se onbtienen unas propiedades que se deben declarar en el servicio para revisar si se esta cargando o hay mas paginas
+const scrollPosition = window.innerHeight + window.scrollY; // el innerheight nos da la altura visible de lo que contiene la ventana, scollY devuelve la cantidad de pixeles que el documento haya recorrido desde su desplazamiento , la suma nos da la cantidad de pixeles recorridos de la parte superior hasta la parte inferior
+const scrollthreshold = document.documentElement.scrollHeight;
+if(scrollPosition >= scrollthreshold){///  el usuario hay llegado al final de la pagina
+  effect(
+    ()=>{
+      this.movies()
+    }
+  )
 }
+}
+// constructor(){
+//  effect(/// ejecuta una funcion segun cambia el signal
+//   ()=>{
+// console.log(this.movies())
+//   }
+//  )
+// }
 
 }
--- apps/diegun-movie/src/app/features/movies/movies.service.ts
+++ apps/diegun-movie/src/app/features/movies/movies.service.ts
@@ -12,6 +12,12 @@ movies = signal<Movie[]>([])
 trendingMovies = signal<Movie[]>([])
 selectedMovie = signal<Movie | null>(null)
 
+//propiedades para las paginas
+currentPage = signal<number>(1);
+hasMorepages = signal<boolean>(true);
+isLoading = signal<boolean>(false);
+
+
 // crear propiedades privadas para el contexto de este fichero
 private readonly apiKey = '19a735cf759a205784acb027f722d718'
 private readonly apiUrl = 'https://api.themoviedb.org/3'
@@ -30,11 +36,20 @@ getMovieById(id: number) : Observable<MovieResponse> {
     return this._http.get<MovieResponse>(`${this.apiUrl}/movie/${id}?api_key=${this.apiKey}`)
 }
 
+
+// privado por ahora para probar, pero puede que toque hacerlo publico segun sea el caso
 private _getMovies() :void{//  aqui obtiene las peliculas y se las asigna a la variable signal movies, para sea usado en cualquier parte
     this._http.get<MovieResponse>(
         `${this.apiUrl}/movie/popular?api_key=${this.apiKey}`
     ).pipe(
-        tap(response => this.movies.set(response.results))/// en la signal de movies se setea el results
+        tap(response =>{
+            const currentMovies = this.movies();
+            this.movies.set([...currentMovies,...response.results])/// se concatena las pelis actuales, mas el resultado
+            this.hasMorepages.set(response.page < response.total_pages)
+            this.currentPage.update((currentPage)=>currentPage +1);
+            this.isLoading.set(false)
+        }
+        )/// en la signal de movies se setea el results
     )
     .subscribe()
 
